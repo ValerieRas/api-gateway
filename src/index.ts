9edbafa14d5bc360ea8d1490cea47054db83c609
valerieRas <rasolovalerie@gmail.com>
2026-06-04 09:47:23 +0200

    refactor: remove config and qcm routes, implement proxy middleware directly in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,24 @@
 import 'dotenv/config'
 import express from 'express'
-import { config } from './config/index.js'
-import { qcmProxy } from './routes/qcm.routes.js'
+import type { NextFunction, Request, Response } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 
 const app = express()
 
-// 1. Logger - Log all incoming requests
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`)
-  next()
-})
+const PORT :number = process.env.PORT ? Number(process.env.PORT) : 3000
+const PORTQCM :number = process.env.PORTQCM ? Number(process.env.PORTQCM) : 3001
 
-// 2. Proxies - SINGLE ENTRY POINT
-// We mount proxies BEFORE express.json() to ensure body streaming is not corrupted
-// All routes starting with /api/qcms are handled by qcmProxy
-app.use(config.services.qcm.prefix, qcmProxy)
 
-// 3. Local Middlewares - Only for routes defined below (local to gateway)
-app.use(express.json())
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: `http://localhost:${PORTQCM}`,
+  changeOrigin: true,
+  pathRewrite: {'^/api/qcms': '/qcms'}
+});
 
-// 4. Gateway Health Check & Info
-app.get('/', (req, res) => {
-  res.json({ 
-    service: 'API Gateway',
-    status: 'UP',
-    version: '1.0.0',
-    routes: [
-      { path: config.services.qcm.prefix, target: 'QCM-Service' }
-    ]
-  })
-})
+app.use('/api', proxyMiddleware);
 
-// 5. Start Server
-app.listen(config.port, '0.0.0.0', () => {
-  console.log(`\n🚀 API Gateway is running on http://localhost:${config.port}`)
-  console.log(`📡 Proxying ${config.services.qcm.prefix}/* to ${config.services.qcm.url}${config.services.qcm.targetPath}/*\n`)
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
